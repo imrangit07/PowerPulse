1ea25de569fd301b5c14d4ac8199dcee45e55177
imrangit07 <imranhussain21309@gmail.com>
2025-03-24 13:37:31 +0530

    add template for items-1 and products1.js file

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 import './style.css'
-import './AutoSwiper';
+import './AutoSwiper'
+
+import products1 from "./Api/Products1.json";
+import { showProducts1Content } from './products/Product1';
+
+// console.log(products1);
+
+// function for the products1
+showProducts1Content(products1)
---
+++ src/products/Product1.js
@@ -0,0 +1,24 @@
+const products1Template = document.querySelector("#products-1--template")
+const item1 = document.querySelector(".items-1")
+
+
+export const showProducts1Content = (product) => {
+    if (!product) {
+        return false;
+    }
+    product.forEach(currentProduct => {
+        const { itemId, imgUrl, deal, title, offIn, off } = currentProduct;
+
+        const productClone = document.importNode(products1Template.content, true);
+
+        console.log(productClone);
+        productClone.querySelector("#product-img").src = imgUrl;
+        productClone.querySelector("#product-new").innerText = deal;
+        productClone.querySelector("#main-product--title").innerText = title;
+        productClone.querySelector("#off-in").innerText = offIn;
+        productClone.querySelector("#off-value").innerHTML = `${off}`;
+        item1.append(productClone);
+
+    });
+
+}
