0040fa5bdb21660cd1b6308d9c2c24ff7dca8cdc
imrangit07 <imranhussain21309@gmail.com>
2025-04-18 11:21:51 +0530

    Add vite.con fiel

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        cart: resolve(__dirname, 'Cart.html'),
+        wishlist: resolve(__dirname, 'wishlist.html'),
+        paymentSuccess: resolve(__dirname, 'paymentsuccess.html'),
+        order: resolve(__dirname, 'order.html')
+      }
+    }
+  }
+});
